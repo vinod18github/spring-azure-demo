38116f0cb410226208a4de5934304937a0e46cb1
VINODKUMAR.NARRA <VINODKUMAR.NARRA@10.48.247.174>
2025-05-14 19:22:22 +0530

    added new message

--- src/main/java/com/example/demo/SpringAzureDemo1Application.java
+++ src/main/java/com/example/demo/SpringAzureDemo1Application.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringAzureDemo1Application {
 
+	@GetMapping("/message")
+	public String message() {
+		return "congrats ! your app deployed in azure successfully";
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureDemo1Application.class, args);
 	}
